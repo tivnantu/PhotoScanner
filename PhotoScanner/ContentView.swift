e8ada0f856fa9e68bfc4ac4a359325f07fb1a653
tivnantu <tivnantu@tencent.com>
2026-03-22 22:39:38 +0800

    Initial Commit

---
+++ PhotoScanner/ContentView.swift
@@ -0,0 +1,25 @@
+//
+// ContentView.swift
+// PhotoScanner
+//
+// Created by tivnantu on 2026/3/22
+// Copyright © 2026 tivnantu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoScanner/PhotoScannerApp.swift
@@ -0,0 +1,18 @@
+//
+// PhotoScannerApp.swift
+// PhotoScanner
+//
+// Created by tivnantu on 2026/3/22
+// Copyright © 2026 tivnantu. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct PhotoScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
